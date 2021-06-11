3b8e6343cc8759e357678ef7c501007043063075
Your Name <takialog@gmail.com>
2021-06-11 10:40:26 +0800

    login form

---
+++ src/components/Form.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <slot name="open-button" :open-dialog="openDialog" />
+    <el-dialog :title="modal" v-model="open" width="30%">
+      <el-form ref="form" label-width="120px">
+        <el-form-item label="User name">
+          <el-input v-model="username"></el-input>
+        </el-form-item>
+        <el-form-item label="Password">
+          <el-input
+            type="password"
+            v-model="password"
+            autocomplete="off"
+          ></el-input>
+        </el-form-item>
+      </el-form>
+      <template #footer>
+        <span class="dialog-footer">
+          <slot name="button"/>
+          <!-- <el-button>Regist</el-button> -->
+        </span>
+      </template>
+    </el-dialog>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType, reactive, toRefs } from "vue";
+
+type form = {
+  username: string;
+  password: string;
+  open: boolean;
+};
+export default defineComponent({
+  props: {
+    modal: { type: String as PropType<"register" | "login"> },
+  },
+  emits: ["welcome"],
+  setup() {
+    const state = reactive<form>({ username: "", password: "", open: false });
+    return { ...toRefs(state) };
+  },
+  methods: {
+    cat() {
+      console.log("cat");
+    },
+    openDialog() {
+      this.open = true;
+    },
+  },
+});
+</script>
+
+<style scoped></style>
---
+++ src/views/Home.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="home">
+    <Form modal="register">
+      <template v-slot:open-button="slotProps">
+        <el-button v-on:click="slotProps.openDialog()">register</el-button>
+      </template>
+      <template v-slot:button>
+        <el-button @click="register">Regist</el-button>
+      </template>
+    </Form>
+    <Form modal="login">
+      <template v-slot:open-button="slotProps">
+        <el-button v-on:click="slotProps.openDialog()">login</el-button>
+      </template>
+      <template v-slot:button>
+        <el-button @click="login">Login</el-button>
+      </template>
+    </Form>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive, toRefs } from "vue";
+import Form from "@/components/Form.vue";
+
+// import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+// type LoginForm = {
+// modal: "none" | "register" | "login";
+// form: {
+//   username: string;
+//   password: string;
+// };
+// };
+
+export default defineComponent({
+  name: "Home",
+  setup() {
+    const state = reactive({});
+
+    return {
+      ...toRefs(state),
+    };
+  },
+  methods: {
+    onSubmit(form: { username: string; password: string }) {
+      console.log("submit!", { form });
+    },
+    register() {
+      console.log("register");
+    },
+    login() {
+      console.log("login");
+    },
+  },
+  components: {
+    Form,
+  },
+});
+</script>
