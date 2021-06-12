8f40dfa150d130f4ab087f047218d29198dc6ccc
Your Name <takialog@gmail.com>
2021-06-12 15:49:48 +0800

    mock server

---
+++ src/test/server/dev-server.js
@@ -0,0 +1,19 @@
+/* eslint-disable */
+/* tslint:disable */
+import {setupWorker} from 'msw'
+import {handlers} from './server-handlers'
+import {homepage} from '../../../package.json'
+
+const fullUrl = new URL(homepage)
+
+const server = setupWorker(...handlers)
+
+server.start({
+  quiet: true,
+  serviceWorker: {
+    url: fullUrl.pathname + 'mockServiceWorker.js',
+  },
+})
+
+export * from 'msw'
+export {server}
---
+++ src/test/server/test-server.js
@@ -0,0 +1,9 @@
+/* eslint-disable */
+/* tslint:disable */
+import {setupServer} from 'msw/node'
+import {handlers} from './server-handlers'
+
+const server = setupServer(...handlers)
+
+export * from 'msw'
+export {server}
