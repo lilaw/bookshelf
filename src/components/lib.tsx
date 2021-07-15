8d00dc8dcc55a460d17689fbdee7af5ff6cb00b2
Your Name <takialog@gmail.com>
2021-07-15 10:08:24 +0800

    feat: loading screen and error layout created

---
+++ src/components/lib.tsx
@@ -0,0 +1,31 @@
+import type {HttpError} from "@/types"
+export const FullPageSpinner = () => {
+  return (
+    <div class="el-loading-mask is-fullscreen" style="z-index: 2001; background-color: rgb(0 0 0);">
+      <div class="el-loading-spinner">
+        <svg class="circular" viewBox="25 25 50 50" aria-label="loading">
+          <circle class="path" cx="50" cy="50" r="20" fill="none"></circle>
+        </svg>
+      </div>
+    </div>
+  );
+};
+
+export function ErrorMessage({error, ...props}: {error: HttpError}) {
+  return (
+    <div
+      role="alert"
+      style={[{color: 'ef5350'}]}
+      {...props}
+    >
+      <span>There was an error: </span>
+      <pre
+        style={[
+          {whiteSpace: 'break-spaces', margin: '0', marginBottom: -5},
+        ]}
+      >
+        {'  '}{error.message}
+      </pre>
+    </div>
+  )
+}
